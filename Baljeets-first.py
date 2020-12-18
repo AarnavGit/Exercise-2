d7a63d2cf78890df88655afacecf6f5d55d92af8
AarnavGit <aarnavtekulapally@gmail.com>
2020-12-17 21:27:37 -0500

    first commit

---
+++ Baljeets-first.py
@@ -0,0 +1,36 @@
+
+
+x = 5
+y = 10
+
+y+= 3
+
+print(y)
+
+x-= 3
+
+print(x)
+
+y *= 3
+
+print(y)
+
+x/= 3
+
+print(x)
+
+x//= 3
+
+print(x)
+y %= 3
+
+print(y)
+
+x**=3
+
+print(x)
+
+print(x == y)
+
+print(x)
+
