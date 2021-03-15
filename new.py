fbfcd516a4c178c9639ec5a3afd0910528531da9
AarnavGit <aarnavtekulapally@gmail.com>
2021-03-14 21:19:03 -0400

    this is my 2nd weeks work

--- new.py
+++ new.py
@@ -1,5 +1,17 @@
+# Week 1
 print("Hello World")
 x =21
 y= 2362984
 print(x)
 print(y)
+# Week 2
+myfirst_list=['apple','orange','tomato']
+print(myfirst_list) 
+
+x=67
+y=21
+
+x+=y
+
+print(x)
+print(x+y)
