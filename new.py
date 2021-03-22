66b63c1a1648fa34f89556d4fdccd009de635ae7
Aarnav-Git <aarnavtekulapally@gmail.com>
2021-03-21 20:28:31 -0400

    this is my week 3 work

--- new.py
+++ new.py
@@ -16,3 +16,11 @@ x+=y
 print(x)
 print(x+y)
 # Week 3
+mylist= ["berries", "watermelon", "banana", "apple"]
+print(mylist[3])
+
+fav_ice_cream= input('Enter your favorite ice cream flavor:')
+if fav_ice_cream == "cookie dough":
+        print("You have the same favorite ice cream!")
+else:
+    print("sorry", fav_ice_cream, "is not my favorite ice cream flavor")
