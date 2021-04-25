0c8299c1334ea21d58e8b7e9ec714386574dba98
AarnavGit <aarnavtekulapally@gmail.com>
2021-04-25 16:29:35 -0400

    This is for ROGATE

---
+++ Drwaing.py
@@ -0,0 +1,11 @@
+import turtle
+
+colors = ["red", "yellow", "green", "purple", "orange"]
+t=turtle.Turtle()
+turtle.bgcolor("black")
+for x in range(360):
+    t.pencolor(colors[x%5])
+    t.width(x/100+1)
+    t.forward(x)
+    t.left(47)
+turtle.done()
